6101ef4915a099ad4b23188d66c0b152378a7d0d
João Marlon <marlonsouto@gmail.com>
2023-11-19 17:03:07 -0300

    refactoring and documentation

--- main.py
+++ main.py
@@ -4,77 +4,112 @@ import re
 
 
 def count_line_cell_notnull(line):
+    """
+    Conta o número de células não nulas em uma linha.
+
+    Parameters:
+        line (pandas.Series): Uma linha do DataFrame.
+
+    Returns:
+        int: Número de células não nulas na linha.
+    """
     return line.count()
 
 
-def group_and_clean_dataframe(title="", dataframe=pd.DataFrame(), columns=list):
-    if columns is None or len(columns) != 0:
-        columns = []
+def clean_title(title):
+    """
+    Limpa e formata um título para ser usado como nome de arquivo.
 
-    if not dataframe.empty:
-        # apaga todas as colunas vazias
-        dataframe = dataframe.dropna(axis=1, how='all')
-        title = title.lower()
-        title = re.sub(r'\s+', ' ', title)
-        title = title.replace(' ', '_')
-        title = re.sub(r'[^a-zA-Z0-9áéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕàèìòÀÈÌÒçÇ _]', '', title)
+    Parameters:
+        title (str): Título a ser limpo.
+
+    Returns:
+        str: Título limpo e formatado.
+    """
+    title = title.lower()
+    title = re.sub(r'\s+', ' ', title)
+    title = title.replace(' ', '_')
+    title = re.sub(r'[^a-zA-Z0-9áéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕàèìòÀÈÌÒçÇ _]', '', title)
+    return title
+
+
+def save_dataframe(dataframe, title):
+    """
+    Salva um DataFrame como um arquivo CSV, se não estiver vazio.
+
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame a ser salvo.
+        title (str): Título para nomear o arquivo.
+
+    Returns:
+        pandas.DataFrame, list: DataFrame vazio e lista vazia.
+    """
+    if not dataframe.empty and not dataframe.columns.empty:
+        title = clean_title(title)
         path = f"./data/{title}.csv"
+        dataframe = dataframe.dropna(axis=1, how='all')
         dataframe.to_csv(path, index=False)
         print(f"Arquivo salvo em {path}")
+        return pd.DataFrame(), []
 
-    dataframe = pd.DataFrame()
-
-    return dataframe, columns
+    return dataframe, []
 
 
 def extract(arquivo_excel):
-    # Use a função ExcelFile para abrir o arquivo Excel
-    xls = pd.ExcelFile(arquivo_excel)
+    """
+    Extrai dados de tabelas de um arquivo Excel e salva como arquivos CSV.
 
-    # Obtenha uma lista de nomes de todas as abas no arquivo
-    nomes_das_abas = xls.sheet_names
+    Parameters:
+        arquivo_excel (str): Caminho para o arquivo Excel a ser processado.
+    """
+    try:
+        # Usando a função ExcelFile para abrir o arquivo Excel
+        xls = pd.ExcelFile(arquivo_excel)
 
-    # Criando um dicionário para armazenar todos os DataFrames das abas
-    planilha = {}
+        # Obtendo uma lista de nomes de todas as abas no arquivo
+        nomes_das_abas = xls.sheet_names
 
-    # Iterando sobre os nomes das abas e leendo os dados em DataFrames
-    for aba in nomes_das_abas:
+        # Criando um dicionário para armazenar todos os DataFrames das abas
+        planilha = {}
 
-        planilha[aba] = pd.read_excel(arquivo_excel, sheet_name=aba, header=None)
+        # Iterando sobre os nomes das abas e lendo os dados em DataFrames
+        for aba in nomes_das_abas:
 
-        dataframe = pd.DataFrame()
-        title = ""
-        columns = []
-        for idx, line in planilha[aba].iterrows():
-            line_size = count_line_cell_notnull(line)
-            if line_size == 0:
-                #chamando o método abaixo para caso pule de uma tabela para outra
-                dataframe, columns = group_and_clean_dataframe(title=title, dataframe=dataframe,
-                                                               columns=columns)
-                continue
-            elif line_size == 1:
-                dataframe, columns = group_and_clean_dataframe(title=title, dataframe=dataframe,
-                                                               columns=columns)
-                # Esse fluxo considera que o texto anterior a tabela dará nome ao arquivo
-                title = f"{aba}_{line.dropna().values[0]}"
-            else:
-                if dataframe.empty and dataframe.columns.empty:
-                    columns = line.values
-                    dataframe = pd.DataFrame(columns=columns)
+            planilha[aba] = pd.read_excel(arquivo_excel, sheet_name=aba, header=None)
+
+            dataframe = pd.DataFrame()
+            title = ""
+            columns = []
+
+            for idx, line in planilha[aba].iterrows():
+                line_size = count_line_cell_notnull(line)
+
+                if line_size == 0:
+                    # chamando o método abaixo para caso pule de uma tabela para outra
+                    dataframe, columns = save_dataframe(dataframe=dataframe, title=title)
+                    continue
+                elif line_size == 1:
+                    dataframe, columns = save_dataframe(dataframe=dataframe, title=title)
+                    # Esse fluxo considera que o texto anterior a tabela dará nome ao arquivo
+                    title = f"{aba}_{line.dropna().values[0]}"
                 else:
-                    new_line = pd.Series(line.values, index=columns)
-                    dataframe = pd.concat([dataframe, new_line.to_frame().T], ignore_index=True)
+                    if dataframe.empty and dataframe.columns.empty:
+                        columns = line.values
+                        dataframe = pd.DataFrame(columns=columns)
+                    else:
+                        new_line = pd.Series(line.values, index=columns)
+                        dataframe = pd.concat([dataframe, new_line.to_frame().T], ignore_index=True)
 
-        group_and_clean_dataframe(title=title, dataframe=dataframe, columns=columns)
+            save_dataframe(dataframe=dataframe, title=title)
+
+    except Exception as e:
+        print(f"Erro durante a execução: {str(e)}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process xlsx files')
-    parser.add_argument('--path', type=str,
-                        help='file that will be analyzed', required=True)
+    parser.add_argument('--path', type=str, help='file that will be analyzed', required=True)
 
     args = parser.parse_args()
 
     extract(args.path)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
